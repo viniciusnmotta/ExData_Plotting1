6b4f1845378e5dd0bde90387363c16a5c8a1267c
viniciusnmotta <Vinicius@Ken-McDonalds-Mac-Mini.local>
2016-01-07 10:40:37 -0500

    Final with .R files with comments

--- figure/Plot2.R
+++ figure/Plot2.R
@@ -1,3 +1,11 @@
+##############################################################################################################
+## Before you can run this script you need to download the text file (Electric power consuption)
+## from: https://www.coursera.org/learn/exploratory-data-analysis/peer/ylVFo/course-project-1
+## and move the file into your working directory together with this script.
+## This script reads the txt file, convert column 1 into Date and subset the data.table based on the date
+## between 2007-02-01 and 2007-02-02. Graph will be plotted using the data.table subsetted from the original file
+##############################################################################################################
+
 df<-read.table("./household_power_consumption.txt",sep=";",header=TRUE,stringsAsFactors = FALSE)
 df[,1]<-as.Date(df[,1],format="%d/%m/%Y")
 df2<-subset(df,Date=="2007-02-01"|Date=="2007-02-02")
